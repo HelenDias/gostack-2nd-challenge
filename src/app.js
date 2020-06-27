f2cd8c4684808977ad2c16f1ab34b53d1ffa5a40
Helen Dias <helendias.hd@gmail.com>
2020-06-27 17:40:58 -0300

    :building_construction: Add logger message on start node

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
+app.get('/repositories', (request, response) => {
   // TODO
-});
+})
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories', (request, response) => {
   // TODO
-});
+})
 
-app.put("/repositories/:id", (request, response) => {
+app.put('/repositories/:id', (request, response) => {
   // TODO
-});
+})
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete('/repositories/:id', (request, response) => {
   // TODO
-});
+})
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
-});
+})
 
-module.exports = app;
+module.exports = app
